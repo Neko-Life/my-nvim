4e08ae2d96da42daa3536ae76e7ef845e1a07438
Neko-Life <nekolife123579@gmail.com>
2022-10-22 10:00:56 +0700

    Initial commit

---
+++ nvim/lua/shasha.lua
@@ -0,0 +1,82 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+require('lspconfig').clangd.setup {
+    capabilities = capabilities,
+}
+
+-- require('lspconfig').quick_lint_js.setup {
+--    capabilities = capabilities,
+-- }
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+local lspkind = require 'lspkind'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+	['<C-d>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<CR>'] = cmp.mapping.confirm {
+	    behavior = cmp.ConfirmBehavior.Replace,
+	    select = true,
+	},
+	['<Tab>'] = cmp.mapping(function(fallback)
+	    if cmp.visible() then
+		cmp.select_next_item()
+	    elseif luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	    else
+		fallback()
+	    end
+	end, { 'i', 's' }),
+	['<S-Tab>'] = cmp.mapping(function(fallback)
+	    if cmp.visible() then
+		cmp.select_prev_item()
+	    elseif luasnip.jumpable(-1) then
+		luasnip.jump(-1)
+	    else
+		fallback()
+	    end
+	end, { 'i', 's' }),
+    }),
+    sources = {
+	{ name = 'nvim_lsp' },
+	{ name = 'luasnip' },
+    },
+    formatting = {
+	format = lspkind.cmp_format {
+	    with_text = true,
+	    menu = {
+		buffer   = "[buf]",
+		nvim_lsp = "[LSP]",
+		path     = "[path]",
+	    },
+	},
+    },
+    experimental = {
+	ghost_text = true
+    }
+}
+
+-- require('lint').linters_by_ft = {
+--   markdown = {'vale',},
+--   cpp = {'clangtidy',},
+--   c = {'clangtidy',},
+--   js = {'eslint',},
+--   ts = {'eslint',},
+-- }
+-- 
+-- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+--   callback = function()
+--     require("lint").try_lint()
+--   end,
+-- })
