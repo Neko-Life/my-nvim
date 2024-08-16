5d65fb1660f009e7e9f32e86321cbede503b4c2c
Neko-Life <nekolife123579@gmail.com>
2024-08-17 00:37:47 +0700

    actually export first

--- config.fish
+++ config.fish
@@ -1,3 +1,7 @@
+# phpenv
+set --export PATH "$HOME/.phpenv/bin" $PATH
+
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
@@ -5,12 +9,10 @@ if status is-interactive
     source (phpenv init -|psub)
 end
 
+
 # Created by `pipx` on 2024-03-17 00:48:39
 set PATH $PATH "$HOME/.local/bin"
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-
-# phpenv
-set --export PATH "$HOME/.phpenv/bin" $PATH
