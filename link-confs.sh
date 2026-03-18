b1435710c6b98366f2b2a32e0270dd0d332ca7f7
Neko-Life <nekolife123579@gmail.com>
2026-03-18 11:58:07 +0700

    update links

--- link-confs.sh
+++ link-confs.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-mkdir -p ~/.config/kitty/ ~/.config/picom/
+mkdir -p ~/.config/kitty/ 	~/.config/picom/
+ln -s "$(pwd)/kitty.conf"	~/.config/kitty/kitty.conf
+ln -s "$(pwd)/picom.conf"	~/.config/picom/picom.conf
 
-ln -s $(pwd)/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $(pwd)/picom.conf ~/.config/picom/picom.conf
+if ( test -d ~/.config/nvim ); then
+	mv ~/.config/nvim ~/.config/nvim-bck
+	echo ~/.config/nvim moved to ~/.config/nvim-bck
+fi
+ln -s "$(pwd)/nvim"		~/.config/nvim
+
+ln -s "$(pwd)/.gitconfig"	~/
+ln -s "$(pwd)/.vimrc"		~/
+ln -s "$(pwd)/.tmux.conf"	~/
+# ln -s "$(pwd)/.Xresources"	~/
+
